737a334d568caf5ba4d78f39d388064763e8f426
vitalves <vitalvesilva@gmail.com>
2019-09-26 19:17:03 -0300

    Carregando dados da API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 // import { Container } from './styles';
 
@@ -8,3 +8,63 @@ export default function Repository({ match }) {
 
   return <h1>Repository { decodeURIComponent(repository) }</h1>;
 }
+*/
+
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+export default class Repository extends Component {
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    // api.github.com/repos/rocketseat/unform
+    // api.github.com/repos/rocketseat/unform/issues
+    /*
+    // EXECUTA UMA A UMA, UMA APOS A OUTRA
+    const response = await api.get(`/repos/${repoName}`);
+    const issues = wait api.get(`/repos/${repoName}/issues`);
+    */
+
+    // EXECUTAM JUNTAS
+    const [ repository, issues ] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        // paramentos de url "/?state=
+        params: {
+          state: 'open',
+          per_page: 5,
+        }
+      })
+    ]);
+
+    // console.log(repository);
+    // console.log(issues);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    })
+
+
+  }
+
+  //
+
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return <h1>Repository</h1>
+  }
+}
+
